4880ecb12b4fd5229ce698c9ea9131c495696b22
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2022-10-19 17:01:10 +0200

    split up paralelization to fix memory issues

--- one_D_model/model/run_SDE_model.py
+++ one_D_model/model/run_SDE_model.py
@@ -21,40 +21,40 @@ SDE_lambda_sol = []
 def main(params):
     # Solve SDEs and run Monte Carlo Simulation
     num_simulation = params.num_simulation
-
+    # ------------------------------------------------------------
     with multiprocessing.Pool(processes=params.num_proc) as pool:
         for res in pool.imap_unordered(partial(solve_SDEs_wrapper, func_name=solve_SDEs.solve_SDE, param=params), range(num_simulation)):
             SDE_delta_T_sol.append(res)
 
+    np.save(params.sol_directory_path + 'SDE_sol_delta_T.npy', SDE_delta_T_sol)
+    # ------------------------------------------------------------
+    with multiprocessing.Pool(processes=params.num_proc) as pool:
+
         for res_u in pool.imap_unordered(partial(solve_SDEs_wrapper, func_name=solve_SDEs.solve_SDE_with_stoch_u, param=params), range(num_simulation)):
             SDE_u_sol.append(res_u)
 
+    SDE_u_sol_delta_T = np.array([SDE_u_sol[idx][0][:] for idx in range(params.num_simulation)])
+    SDE_u_sol_u = np.array([SDE_u_sol[idx][1][:] for idx in range(params.num_simulation)])
+
+    np.save(params.sol_directory_path + 'SDE_u_sol_delta_T.npy', SDE_u_sol_delta_T)
+    np.save(params.sol_directory_path + 'SDE_u_sol_u.npy', SDE_u_sol_u)
+    # ------------------------------------------------------------
+    with multiprocessing.Pool(processes=params.num_proc) as pool:
         for res_Qi in pool.imap_unordered(partial(solve_SDEs_wrapper, func_name=solve_SDEs.solve_SDE_with_stoch_Qi, param=params), range(num_simulation)):
             SDE_Qi_sol.append(res_Qi)
 
+    SDE_Qi_sol_delta_T = np.array([SDE_Qi_sol[idx][0][:] for idx in range(params.num_simulation)])
+    SDE_Qi_sol_Qi = np.array([SDE_Qi_sol[idx][1][:] for idx in range(params.num_simulation)])
+
+    np.save(params.sol_directory_path + 'SDE_Qi_sol_delta_T.npy', SDE_Qi_sol_delta_T)
+    np.save(params.sol_directory_path + 'SDE_Qi_sol_Qi.npy', SDE_Qi_sol_Qi)
+    # ------------------------------------------------------------
+    with multiprocessing.Pool(processes=params.num_proc) as pool:
         for res_lambda in pool.imap_unordered(partial(solve_SDEs_wrapper, func_name=solve_SDEs.solve_SDE_with_stoch_lambda, param=params), range(num_simulation)):
             SDE_lambda_sol.append(res_lambda)
 
-        print(np.shape(SDE_delta_T_sol))
-        print(np.shape(SDE_u_sol))
-        print(np.shape(SDE_Qi_sol))
-        print(np.shape(SDE_lambda_sol))
+    SDE_lambda_sol_delta_T = np.array([SDE_lambda_sol[idx][0][:] for idx in range(params.num_simulation)])
+    SDE_lambda_sol_lambda = np.array([SDE_lambda_sol[idx][1][:] for idx in range(params.num_simulation)])
 
-        # Save simulation output
-        SDE_u_sol_delta_T = np.array([SDE_u_sol[idx][0][:] for idx in range(params.num_simulation)])
-        SDE_u_sol_u = np.array([SDE_u_sol[idx][1][:] for idx in range(params.num_simulation)])
-        SDE_Qi_sol_delta_T = np.array([SDE_Qi_sol[idx][0][:] for idx in range(params.num_simulation)])
-        SDE_Qi_sol_Qi = np.array([SDE_Qi_sol[idx][1][:] for idx in range(params.num_simulation)])
-        SDE_lambda_sol_delta_T = np.array([SDE_lambda_sol[idx][0][:] for idx in range(params.num_simulation)])
-        SDE_lambda_sol_lambda = np.array([SDE_lambda_sol[idx][1][:] for idx in range(params.num_simulation)])
-
-        np.save(params.sol_directory_path + 'SDE_sol_delta_T.npy', SDE_delta_T_sol)
-
-        np.save(params.sol_directory_path + 'SDE_u_sol_delta_T.npy', SDE_u_sol_delta_T)
-        np.save(params.sol_directory_path + 'SDE_u_sol_u.npy', SDE_u_sol_u)
-
-        np.save(params.sol_directory_path + 'SDE_Qi_sol_delta_T.npy', SDE_Qi_sol_delta_T)
-        np.save(params.sol_directory_path + 'SDE_Qi_sol_Qi.npy', SDE_Qi_sol_Qi)
-
-        np.save(params.sol_directory_path + 'SDE_lambda_sol_delta_T.npy', SDE_lambda_sol_delta_T)
-        np.save(params.sol_directory_path + 'SDE_lambda_sol_lambda.npy', SDE_lambda_sol_lambda)
+    np.save(params.sol_directory_path + 'SDE_lambda_sol_delta_T.npy', SDE_lambda_sol_delta_T)
+    np.save(params.sol_directory_path + 'SDE_lambda_sol_lambda.npy', SDE_lambda_sol_lambda)
