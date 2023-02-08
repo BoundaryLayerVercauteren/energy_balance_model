ffefa43650735e083273db9d85514ff36efe0a06
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-02-08 15:43:18 +0100

    added function to have potential and histogram in one plot

--- one_D_model/utils/plot_output.py
+++ one_D_model/utils/plot_output.py
@@ -23,7 +23,7 @@ plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 
-def make_2D_plot(params, x, y, file_name, xlabel='t [h]', ylabel=r'$\Delta T$ [K]', ylim=(0,40)):
+def make_2D_plot(params, x, y, file_name, xlabel='t [h]', ylabel=r'$\Delta T$ [K]', ylim=(0, 40)):
     color = matplotlib.cm.get_cmap('cmc.batlow', 1).colors
     fig = plt.figure(figsize=(15, 5))
     ax1 = fig.add_subplot(1, 1, 1)
@@ -43,7 +43,8 @@ def make_2D_plot(params, x, y, file_name, xlabel='t [h]', ylabel=r'$\Delta T$ [K
     plt.close('all')  # Closes all the figure windows.
 
 
-def make_2D_multi_line_plot(params, x, y_array, labels, file_name, xlabel='u [m/s]', ylabel=r'$\Delta T_{eq}$ [K]', ylim=(0,30)):
+def make_2D_multi_line_plot(params, x, y_array, labels, file_name, xlabel='u [m/s]', ylabel=r'$\Delta T_{eq}$ [K]',
+                            ylim=(0, 30)):
     color = matplotlib.cm.get_cmap('cmc.batlow', np.shape(y_array)[1] + 1).colors
     fig = plt.figure(figsize=(15, 5))
     ax1 = fig.add_subplot(1, 1, 1)
@@ -98,23 +99,64 @@ def plot_potentials(param_class):
     for idx, u_elem in enumerate(u_list_st):
         param_copy.stab_func_type = 'short_tail'
         potential_st = solve_ODE.calculate_potential(delta_T_range, u_elem, param_copy)
-        ax[0].plot(delta_T_range, - potential_st, label='u = ' + str(u_elem), color=color[idx], marker=markers[idx], markevery=5)
+        ax[0].plot(delta_T_range, - potential_st, label='u = ' + str(u_elem), color=color[idx], marker=markers[idx],
+                   markevery=5)
 
     ax[0].set_xlabel('$\Delta T$ [K]')
     ax[0].set_ylabel('V [$K^2$/s]')
-    #ax[0].set_ylim(-0.25, 0.15)
+    # ax[0].set_ylim(-0.25, 0.15)
     ax[0].legend()
     ax[0].set_title('a)', loc='left')
 
     for idx, u_elem in enumerate(u_list_lt):
         param_copy.stab_func_type = 'long_tail'
         potential_lt = solve_ODE.calculate_potential(delta_T_range, u_elem, param_copy)
-        ax[1].plot(delta_T_range[8:44], - potential_lt[8:44], label='u = ' + str(u_elem), color=color[idx], marker=markers[idx], markevery=5)
+        ax[1].plot(delta_T_range[8:44], - potential_lt[8:44], label='u = ' + str(u_elem), color=color[idx],
+                   marker=markers[idx], markevery=5)
 
     ax[1].set_xlabel('$\Delta T$ [K]')
     ax[1].set_ylabel('V [$K^2$/s]')
-    #ax[1].set_ylim(-0.25/2, 0.15/2)
+    # ax[1].set_ylim(-0.25/2, 0.15/2)
     ax[1].legend()
     ax[1].set_title('b)', loc='left')
 
     plt.savefig(param_class.sol_directory_path + 'potentials.png', bbox_inches='tight', dpi=300)
+
+
+def plot_potentials_and_output_distribution(param_class, delta_T_data):
+    delta_T_range = np.arange(0, 30, 0.5)
+    u_list_st = [5.3, 5.6, 5.9]
+    u_list_lt = [4.87, 4.89, 4.9]
+
+    color = matplotlib.cm.get_cmap('cmc.batlow', 4).colors
+    markers = ['s', 'p', 'v']
+
+    # copy dataclass to prevent overwriting original
+    param_copy = dataclasses.replace(param_class)
+
+    fig, ax1 = plt.subplots(1, 1, figsize=(5, 5))
+    fig.subplots_adjust(right=0.75)
+
+    ax2 = ax1.twinx()
+
+    if param_copy.stab_func_type == 'short_tail':
+        p0 = ax2.hist(delta_T_data, 100, color=color[1], alpha=0.5)
+        for idx, u_elem in enumerate(u_list_st):
+            potential_st = solve_ODE.calculate_potential(delta_T_range, u_elem, param_copy)
+            p1, = ax1.plot(delta_T_range, - potential_st, label='u = ' + str(u_elem), color=color[idx],
+                           marker=markers[idx], markevery=5)
+
+    elif param_copy.stab_func_type == 'long_tail':
+        p2 = ax2.hist(delta_T_data, 100, color=color[1], alpha=0.5)
+        for idx, u_elem in enumerate(u_list_lt):
+            potential_lt = solve_ODE.calculate_potential(delta_T_range, u_elem, param_copy)
+            p3, = ax1.plot(delta_T_range, - potential_lt, label='u = ' + str(u_elem), color=color[idx],
+                           marker=markers[idx], markevery=5)
+
+        ax1.set_xlabel('$\Delta T$ [K]')
+        ax1.set_ylabel('V [$K^2$/s]')
+        ax2.set_ylabel(r'Density of $\Delta T$', color=color[1])
+        ax2.tick_params(axis="y", labelcolor=color[1])
+
+    plt.savefig(param_class.sol_directory_path + 'data_dist_potentials_' + param_copy.stab_func_type + '.png',
+                bbox_inches='tight', dpi=300)
