ff8ef3fc82dd93e8c5b4723292b34303da0ce10e
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-02-21 13:31:11 +0100

    dont upload data files

--- DomeC/bin_data.py
+++ DomeC/bin_data.py
@@ -1,36 +1,50 @@
 import numpy as np
-
+import pandas as pd
+import seaborn as sns
+from scipy import stats
 from process_dome_c_data import prepare_dome_c_data
 import matplotlib.pyplot as plt
 import matplotlib
 import cmcrameri.cm as cmc
 
 
-def find_data_in_u_bin(data, bin_mean=4.5, bin_var=1.5):
-    data.loc[(data['U2[m s-1]'] < (bin_mean - bin_var)) | (data['U2[m s-1]'] > (bin_mean + bin_var))] = np.nan
+def find_data_in_u_bin(data, bin_mean=5.6, bin_var=1.0):
+    if 'U2[m s-1]' in data.columns:
+        data.loc[(data['U2[m s-1]'] < (bin_mean - bin_var)) | (data['U2[m s-1]'] > (bin_mean + bin_var))] = np.nan
+    else:
+        data.loc[(data['U_9m [m/s]'] < (bin_mean - bin_var)) | (
+                    data['U_9m [m/s]'] > (bin_mean + bin_var)), 'U_9m [m/s]'] = np.nan
     return data
 
 
-def find_longest_continous_time_series(data):
+def find_longest_continous_time_series(data, year):
+    # Select rows which are in given year
+    data.loc[data['year'] != year, 'tempInv [K]'] = np.nan
+    # Select rows where U is not NaN
+    if 'U2[m s-1]' in data.columns:
+        data.loc[data['U2[m s-1]'].isnull(), 'tempInv [K]'] = np.nan
+    else:
+        data.loc[data['U_9m [m/s]'].isnull(), 'tempInv [K]'] = np.nan
     data_values = data['tempInv [K]'].values
     # Mask values
     masked_values = np.concatenate(([True], np.isnan(data_values), [True]))
     # Find start-stop limits (adjust index to fit input dataframe)
     ss = np.flatnonzero(masked_values[1:] != masked_values[:-1]).reshape(-1, 2)  # + data.index.min()
-    # Find index of 10 longest time series
+    # Find length of time series
     ts_length_start_stop = []
     for row in ss:
         ts_length_start_stop.append((row[1] - row[0], row))
     # Sort time series start stop values by length
     ts_length_start_stop = sorted(ts_length_start_stop, key=lambda x: x[0])
     # Get 10 longest time series
-    start_stop_longest_ts = ts_length_start_stop[-20:]
-    return start_stop_longest_ts
+    start_stop_longest_ts = ts_length_start_stop[-10:]
+
+    return start_stop_longest_ts, ts_length_start_stop
 
 
-def check_for_transition(data, start_top_cons):
+def check_for_transition(data, longest_cons_deltaT):
     start_stop_with_trans = []
-    for idx in np.arange(0, len(start_top_cons)):
+    for idx in np.arange(0, len(longest_cons_deltaT)):
         curr_data = data['tempInv [K]'].iloc[longest_cons_deltaT[idx][1][0]:longest_cons_deltaT[idx][1][1]]
         if curr_data.max() >= 10 and curr_data.min() <= 5:
             start_stop_with_trans.append((longest_cons_deltaT[idx][1][0], longest_cons_deltaT[idx][1][1]))
@@ -39,138 +53,115 @@ def check_for_transition(data, start_top_cons):
 
 if __name__ == "__main__":
     # Prepare data
-    _, data_season = prepare_dome_c_data()
+    data_season = prepare_dome_c_data()
+
+    # Fill jumps in time with nan
+    # Be aware that this changes the index
+    data_season = data_season.set_index('timestamp').resample('30Min').mean().reset_index()
+    jump = np.abs(data_season.timestamp - data_season.timestamp.shift(1))
+
+    # Add year column
+    data_season['year'] = pd.DatetimeIndex(data_season['timestamp']).year
+
     # Select data where u is in specific bin
     data_in_u_bin = find_data_in_u_bin(data_season.copy())
-    # Select 10 longest time series with no missing delta T (returns start and stop index of time series)
-    longest_cons_deltaT = find_longest_continous_time_series(data_in_u_bin)
-    # Only select time series with a transition
-    ss_idx_with_trans = check_for_transition(data_in_u_bin, longest_cons_deltaT)
 
-    # # Plot histogram
-    # fig = plt.figure(figsize=(5, 5))
-    # plt.hist(data_in_u_bin['tempInv [K]'], 10)
-    # plt.ylabel(r'Density of $\Delta T$')
-    # plt.xlabel(r'$\Delta T$ [K]')
-    # plt.savefig('DomeC/figures/hist_domeC_u_bin.png', bbox_inches='tight', dpi=300)
-    #
-    # # Plot data over time
-    # fig = plt.figure(figsize=(10, 5))
-    # plt.plot(data_in_u_bin['Local Time (UTC+8h)'], data_in_u_bin['tempInv [K]'])
-    # plt.ylabel(r'$\Delta T$ [K]')
-    # plt.ylabel(r'time')
-    # # plt.xticks(rotation=90)
-    # plt.savefig('DomeC/figures/domeC_u_bin_over_t.png', bbox_inches='tight', dpi=300)
-    #
-    # # Plot data over u
-    # fig = plt.figure(figsize=(10, 5))
-    # plt.axvline(x=5.6, color='r')
-    # plt.axvline(x=5.1, color='r', linestyle='dashed')
-    # plt.axvline(x=6.1, color='r', linestyle='dashed')
-    # plt.scatter(data_season['U2[m s-1]'], data_season['tempInv [K]'], s=5)
-    # plt.ylabel(r'$\Delta T$ [K]')
-    # plt.xlabel(r'u [m/s]')
-    # plt.savefig('DomeC/figures/domeC_deltaT_over_u.png', bbox_inches='tight', dpi=300)
+    # # Plot data over time for every year
+    # g = sns.FacetGrid(data=data_in_u_bin, col='year', col_wrap=4, height=4, aspect=3/2, sharey=True, sharex=False)
+    # g.map_dataframe(sns.lineplot, x='timestamp', y='tempInv [K]')
+    # plt.savefig('DomeC/figures/domeC_u_binned_deltaT_over_time.png', bbox_inches='tight', dpi=300)
 
-    # # Plot data over time
-    # for idx in np.arange(0, len(ss_idx_with_trans)):
-    #     fig = plt.figure(figsize=(10, 5))
-    #     plt.plot(data_in_u_bin['Local Time (UTC+8h)'].iloc[
-    #              ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]],
-    #              data_in_u_bin['tempInv [K]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]])
-    #     plt.ylabel(r'$\Delta T$ [K]')
-    #     plt.xlabel(r'time')
-    #     plt.savefig('DomeC/figures/domeC_u_bin_longest_ts_over_t_' + str(idx) + '.png', bbox_inches='tight', dpi=300)
-    #
-    # # Plot data over u
-    # for idx in np.arange(0, len(ss_idx_with_trans)):
-    #     fig = plt.figure(figsize=(10, 5))
-    #     plt.scatter(
-    #         data_in_u_bin['U2[m s-1]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]],
-    #         data_in_u_bin['tempInv [K]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]])#, s=5)
-    #     # plt.axvline(x=5.6, color='r')
-    #     # plt.axvline(x=5.1, color='r', linestyle='dashed')
-    #     # plt.axvline(x=6.1, color='r', linestyle='dashed')
-    #     plt.ylabel(r'$\Delta T$ [K]')
-    #     plt.xlabel(r'u [m/s]')
-    #     plt.savefig('DomeC/figures/domeC_u_bin_longest_ts_over_u_' + str(idx) + '.png', bbox_inches='tight', dpi=300)
+    # # Plot histogram for every year
+    # facet_kws = {'sharey': False, 'sharex': True}
+    # sns.displot(data=data_in_u_bin, x='tempInv [K]', col='year', col_wrap=4, height=4, aspect=1, facet_kws=facet_kws,
+    #             kde=True)
+    # plt.savefig('DomeC/figures/hist_deltaT_domeC_u_bin.png', bbox_inches='tight', dpi=300)
 
-    # # Plot data over time
-    # for idx in np.arange(0, len(ss_idx_with_trans)):
-    #     # Calculate net radiation
-    #     SW_radiation = data_in_u_bin['SWdn[W m-2]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]].values - \
-    #                    data_in_u_bin['SWup[W m-2]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]].values
-    #     LW_radiation = data_in_u_bin['LWdn[W m-2]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]].values - \
-    #                    data_in_u_bin['LWup[W m-2]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]].values
-    #
-    #     fig = plt.figure(figsize=(10, 5))
-    #     plt.plot(
-    #         data_in_u_bin['Local Time (UTC+8h)'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]],
-    #         SW_radiation + LW_radiation, label='net radiation')
-    #     plt.plot(
-    #         data_in_u_bin['Local Time (UTC+8h)'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]],
-    #         SW_radiation, label='short-wave radiation')
-    #     plt.plot(
-    #         data_in_u_bin['Local Time (UTC+8h)'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]],
-    #         LW_radiation, label='long-wave radiation')
-    #     plt.ylabel(r'R [W/$m^2$]')
-    #     plt.xlabel(r'u [m/s]')
-    #     plt.legend()
-    #     plt.savefig('DomeC/figures/domeC_u_bin_radiation_longest_ts_' + str(idx) + '.png', bbox_inches='tight', dpi=300)
+    # Select longest times series with transitions for specified years and plot them
+    years = [2010, 2012, 2014, 2016, 2017, 2018, 2019]
+    ss_idx_with_trans = np.zeros((len(years), 20), dtype="f,f")
+    for year_idx, year in enumerate(years):
+        # Select longest time series (plural) with no missing delta T (returns start and stop index of time series)
+        longest_cons_deltaT, all_cons_deltaT = find_longest_continous_time_series(data_in_u_bin.copy(), year)
 
-    # Plot data over time
-    for idx in np.arange(0, len(ss_idx_with_trans)):
-        # Calculate net radiation
-        SW_radiation = data_in_u_bin['SWdn[W m-2]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]].values - \
-                       data_in_u_bin['SWup[W m-2]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]].values
-        LW_radiation = data_in_u_bin['LWdn[W m-2]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]].values - \
-                       data_in_u_bin['LWup[W m-2]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]].values
+        fig = plt.figure(figsize=(15, 5))
+        ax1 = fig.add_subplot(1, 1, 1)
+        ax1.plot(data_in_u_bin.loc[data_in_u_bin['year'] == year]['timestamp'],
+                 data_in_u_bin.loc[data_in_u_bin['year'] == year]['tempInv [K]'], color='black')
 
-        color = matplotlib.cm.get_cmap('cmc.batlow', 6).colors
-        markers = ['v', 's', 'p', '*', '^', '.']
+        for elem_idx, elem in enumerate(longest_cons_deltaT):
+            ax1.plot(data_in_u_bin.iloc[elem[1][0]:elem[1][1]]['timestamp'],
+                     data_in_u_bin.iloc[elem[1][0]:elem[1][1]]['tempInv [K]'], color='red')
 
-        fig, ax1 = plt.subplots(figsize=(10, 5))
-        fig.subplots_adjust(right=0.75)
-        fig.subplots_adjust(right=0.75)
+        # Only select time series with a transition
+        trans_ts = check_for_transition(data_in_u_bin, longest_cons_deltaT)
 
-        ax2 = ax1.twinx()
-        ax3 = ax1.twinx()
+        for elem_idx, elem in enumerate(trans_ts):
+            ss_idx_with_trans[year_idx, elem_idx] = elem
+            ax1.plot(data_in_u_bin.iloc[elem[0]:elem[1]]['timestamp'],
+                     data_in_u_bin.iloc[elem[0]:elem[1]]['tempInv [K]'], color='green')
 
-        ax3.spines.right.set_position(("axes", 1.2))
+        plt.savefig('DomeC/figures/domeC_u_binned_deltaT_over_time_marked_trans_' + str(year) + '.png',
+                    bbox_inches='tight', dpi=300)
 
-        p1, = ax1.plot(
-            data_in_u_bin['Local Time (UTC+8h)'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]],
-            SW_radiation + LW_radiation, label='net radiation', color=color[0], marker=markers[0], markevery=10)
-        p2, = ax1.plot(
-            data_in_u_bin['Local Time (UTC+8h)'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]],
-            SW_radiation, label='short-wave radiation', color=color[1], marker=markers[1], markevery=10)
-        p3, = ax1.plot(
-            data_in_u_bin['Local Time (UTC+8h)'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]],
-            LW_radiation, label='long-wave radiation', color=color[2], marker=markers[2], markevery=10)
-        p4, = ax2.plot(data_in_u_bin['Local Time (UTC+8h)'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]],
-                 data_in_u_bin['tempInv [K]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]],
-                 label=r'$\Delta \theta$ [K]', color='red', marker=markers[4], markevery=10)
-        p5, = ax3.plot(data_in_u_bin['Local Time (UTC+8h)'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]],
-                 data_in_u_bin['U2[m s-1]'].iloc[ss_idx_with_trans[idx][0]:ss_idx_with_trans[idx][1]],
-                 label=r'u', color=color[4], marker=markers[5], markevery=10)
+    # Select times series with transitions separately for specified years and plot them over time
+    for year_idx, year in enumerate(years):
+        fig, axs = plt.subplots(2,3, figsize=(10, 5))
+        fig.suptitle(str(year), fontsize=16)
+        fig.subplots_adjust(hspace=.5, wspace=.001)
+        axs = axs.ravel()
+        for elem_idx, elem in enumerate(ss_idx_with_trans[year_idx]):
+            if elem[0] != 0.0 and elem[1] != 0.0:
+                axs[elem_idx].plot(data_in_u_bin.iloc[int(elem[0]):int(elem[1])]['timestamp'],
+                                   data_in_u_bin.iloc[int(elem[0]):int(elem[1])]['tempInv [K]'], color='blue')
+                if 'U2[m s-1]' in data_in_u_bin.columns:
+                    axs[elem_idx].plot(data_in_u_bin.iloc[int(elem[0]):int(elem[1])]['timestamp'],
+                                       data_in_u_bin.iloc[int(elem[0]):int(elem[1])]['U2[m s-1]'], color='green')
+                else:
+                    axs[elem_idx].plot(data_in_u_bin.iloc[int(elem[0]):int(elem[1])]['timestamp'],
+                                       data_in_u_bin.iloc[int(elem[0]):int(elem[1])]['U_9m [m/s]'], color='green')
+                axs[elem_idx].tick_params(labelrotation=90)
 
-        ax1.set_ylabel(r'R [W/$m^2$]')
-        ax2.set_ylabel(r'$\Delta \theta$ [K]', color=p4.get_color())
-        ax3.set_ylabel(r'u[m/s]', color=p5.get_color())
+        fig.tight_layout()
+        fig.subplots_adjust(top=0.88)
 
-        ax2.tick_params(axis="y", labelcolor=p4.get_color())
-        ax3.tick_params(axis="y", labelcolor=p5.get_color())
+        plt.savefig('DomeC/figures/domeC_u_binned_deltaT_over_time_sep_trans_' + str(year) + '.png',
+                    bbox_inches='tight', dpi=300)
 
-        ax1.set_xlabel(r'time [h]')
+    # Select times series with transitions separately for specified years and plot histogram of them
+    for year_idx, year in enumerate(years):
+        fig, axs = plt.subplots(2,3, figsize=(10, 5))
+        fig.suptitle(str(year), fontsize=16)
+        fig.subplots_adjust(hspace=.5, wspace=.001)
+        axs = axs.ravel()
+        for elem_idx, elem in enumerate(ss_idx_with_trans[year_idx]):
+            if elem[0] != 0.0 and elem[1] != 0.0:
+                deltaT_val = data_in_u_bin.iloc[int(elem[0]):int(elem[1])]['tempInv [K]']
+                axs[elem_idx].hist(deltaT_val, bins=20, density=True)
+                sns.kdeplot(deltaT_val, ax=axs[elem_idx])
 
-        ax1.legend(handles=[p1, p2, p3, p4, p5])
+        fig.tight_layout()
+        fig.subplots_adjust(top=0.88)
 
-        plt.savefig('DomeC/figures/domeC_u_bin_combined_longest_ts_' + str(idx) + '.png', bbox_inches='tight', dpi=300)
+        plt.savefig('DomeC/figures/domeC_u_binned_deltaT_hist_sep_trans_' + str(year) + '.png',
+                    bbox_inches='tight', dpi=300)
+    exit()
+    # Save selected time series in file
 
-    # # Save one time series to use as input for model
-    # index = 3
-    # np.savetxt(r'DomeC/data/u_values.txt',
-    #            data_in_u_bin['U2[m s-1]'].iloc[ss_idx_with_trans[index][0]:ss_idx_with_trans[index][1]].values)
-    # np.savetxt(r'DomeC/data/delta_theta_values.txt',
-    #            data_in_u_bin['tempInv [K]'].iloc[ss_idx_with_trans[index][0]:ss_idx_with_trans[index][1]].values)
-    # np.savetxt(r'DomeC/data/net_radiation_values.txt', net_radiation)
+    np.savetxt('DomeC/data/deltaT_values_2018_3.txt',
+               data_in_u_bin.iloc[int(ss_idx_with_trans[3][3][0]):int(ss_idx_with_trans[3][3][1])]['tempInv [K]'].values)
+    np.savetxt('DomeC/data/deltaT_values_2019_2.txt',
+               data_in_u_bin.iloc[int(ss_idx_with_trans[4][2][0]):int(ss_idx_with_trans[4][2][1])]['tempInv [K]'].values)
+
+    if 'U2[m s-1]' in data_in_u_bin.columns:
+        np.savetxt('DomeC/data/u_values_2018_3.txt',
+                   data_in_u_bin.iloc[int(ss_idx_with_trans[3][3][0]):int(ss_idx_with_trans[3][3][1])][
+                       'U2[m s-1]'].values)
+        np.savetxt('DomeC/data/u_values_2019_2.txt',
+                   data_in_u_bin.iloc[int(ss_idx_with_trans[4][2][0]):int(ss_idx_with_trans[4][2][1])][
+                       'U2[m s-1]'].values)
+    else:
+        np.savetxt('DomeC/data/u_values_2018_3.txt',
+                   data_in_u_bin.iloc[int(ss_idx_with_trans[3][3][0]):int(ss_idx_with_trans[3][3][1])]['U_9m [m/s]'].values)
+        np.savetxt('DomeC/data/u_values_2019_2.txt',
+                   data_in_u_bin.iloc[int(ss_idx_with_trans[4][2][0]):int(ss_idx_with_trans[4][2][1])]['U_9m [m/s]'].values)
--- DomeC/process_dome_c_data.py
+++ DomeC/process_dome_c_data.py
@@ -5,7 +5,31 @@ import numpy as np
 
 def load_data(file_name):
     # load data and skip comments in header
-    return pd.read_csv(file_name, sep='\t', skiprows=32, index_col=False, na_values='NaN')
+    if 'ascii' in file_name:
+        values = pd.read_csv(file_name, sep='\t', skiprows=32, index_col=False, na_values='NaN')
+    elif 'tab' in file_name:
+        values = pd.read_csv(file_name, sep='\t', skiprows=26, index_col=False, na_values='NaN')
+    return values
+
+
+def rename_columns(values, file_name):
+    if 'ascii' in file_name:
+        values = values.rename(columns={'Local Time (UTC+8h)': 'timestamp'})
+    elif 'tab' in file_name:
+        values = values.rename(columns={'Date/Time local (local time (UTC+08))': 'timestamp',
+                                        'TTT [°C] (at 3m mean height above groun...)': 'T_3m [oC]',
+                                        'TTT [°C] (at 9m mean height above groun...)': 'T_9m [oC]',
+                                        'TTT [°C] (at 17m mean height above grou...)': 'T_17m [oC]',
+                                        'TTT [°C] (at 25m mean height above grou...)': 'T_25m [oC]',
+                                        'TTT [°C] (at 32m mean height above grou...)': 'T_32m [oC]',
+                                        'TTT [°C] (at 40m mean height above grou...)': 'T_40m [oC]',
+                                        'ff [m/s] (at 3m mean height above groun...)': 'U_3m [m/s]',
+                                        'ff [m/s] (at 9m mean height above groun...)': 'U_9m [m/s]',
+                                        'ff [m/s] (at 17m mean height above grou...)': 'U_17m [m/s]',
+                                        'ff [m/s] (at 25m mean height above grou...)': 'U_25m [m/s]',
+                                        'ff [m/s] (at 32m mean height above grou...)': 'U_32m [m/s]',
+                                        'ff [m/s] (at 40m mean height above grou...)': 'U_40m [m/s]'})
+    return values
 
 
 def make_data_type_numeric(values):
@@ -18,17 +42,21 @@ def convert_temp_unit(values):
     return values + 273.15
 
 
-def convert_to_data_format(values):
+def convert_to_datetime_format(values):
     # convert local time to date format
-    return pd.to_datetime(values, format='%Y-%m-%d_%H:%M:%S')
+    try:
+        values = pd.to_datetime(values, format='%Y-%m-%d_%H:%M:%S')
+    except Exception:
+        values = pd.to_datetime(values, format='%Y-%m-%dT%H:%M:%S')
+    return values
 
 
 def select_time_period(values, period):
     # select only data for given month interval
     if len(period) == 2:
-        return values.loc[values['Local Time (UTC+8h)'].dt.month.between(period[0], period[1]), values.columns]
+        return values.loc[values['timestamp'].dt.month.between(period[0], period[1]), values.columns]
     else:
-        return values[(values['Local Time (UTC+8h)'] >= '2017-07-01 00:00:00') & (values['Local Time (UTC+8h)'] <= '2017-07-15 23:50:00')]
+        return values[(values['timestamp'] >= '2017-07-01 00:00:00') & (values['timestamp'] <= '2017-07-15 23:50:00')]
 
 
 def calculate_rad_force(values):
@@ -38,42 +66,56 @@ def calculate_rad_force(values):
 
 
 def calculate_temp_inv(values):
-    # calculate temperature difference
-    values['tempInv [K]'] = values['T2[K]'] - values['TS[K]']
+    # Calculate temperature difference
+    if 'T2[K]' in values.columns:
+        values['tempInv [K]'] = values['T2[K]'] - values['TS[K]']
+    else:
+        values['tempInv [K]'] = values['T_9m [K]'] - values['T_3m [K]']
     return values
 
 
 def prepare_dome_c_data():
     """prepare data so that it can be used."""
-    # load data
-    input_file = 'DomeC/data/DC_2017_10min.ascii'
-    data = load_data(input_file)
-    # convert column content to correct format
+    # Load data
+    #input_file = 'DomeC/data/10min/DC_2017_10min.ascii'
+    input_file_temp = 'DomeC/data/30min/Genthon-etal_2021_DomeC-Temp.tab'
+    input_file_wind = 'DomeC/data/30min/Genthon-etal_2021_DomeC-WindSpeed.tab'
+    # data = load_data(input_file)
+    data_temp = load_data(input_file_temp)
+    data_wind = load_data(input_file_wind)
+    data = data_temp.join(data_wind.set_index('Date/Time local (local time (UTC+08))'), on='Date/Time local (local time (UTC+08))')
+    # Rename columns
+    data = rename_columns(data, input_file_temp)
+    # Convert column content to correct format
     for i in data.columns[1:]:
         data[i] = pd.to_numeric(data[i], errors='coerce')
         if 'oC' in i:
             data[i] = convert_temp_unit(data[i])
             data = data.rename(columns={i: i.replace('oC', 'K')})
-    data.iloc[:, 0] = convert_to_data_format(data.iloc[:, 0])
-    # calculate radiative forcing
-    data = calculate_rad_force(data)
-    # calculate values for temperature inversion
+    # Convert timestamp column to correct format
+    data.iloc[:, 0] = convert_to_datetime_format(data.iloc[:, 0])
+    # # calculate radiative forcing
+    # data = calculate_rad_force(data)
+    # Calculate values for temperature inversion
     data = calculate_temp_inv(data)
-    # select subset where the forcing is less than a given value and from a specific time period
-    data.loc[data['radForce'] >= 80, data.columns] = np.nan
+    # # select subset where the forcing is less than a given value and from a specific time period
+    # data.loc[data['radForce'] >= 80, data.columns] = np.nan
     sub_data = data.copy()
-    sub_data_day = select_time_period(sub_data, 'none')
-    sub_data_season = select_time_period(sub_data, [5,8])
+    sub_data_season = select_time_period(sub_data, [3,9])
 
-    return sub_data_day, sub_data_season
+    return sub_data_season
 
 
-def main(params):
+def main():
     # Prepare data
-    data_day, data_season = prepare_dome_c_data()
+    data_season = prepare_dome_c_data()
     # Plot temperature inversion over time
     #plot.make_2D_plot(params, data_day['Local Time (UTC+8h)'], data_day['tempInv [K]'], 'dome_c_day.png', xlabel='t [d]', ylabel=r'$\Delta T$ [K]')
     # Plot distribution for whole season
     #plot.make_distribution_plot(data_season['tempInv [K]'], params, 'dome_c_season_distribution.png', r'$\Delta T$ [K]')
 
     return data_season
+
+
+if __name__ == "__main__":
+    main()
