7ded9d0790c17e54838c0642ff8c2e50bc02b01c
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-03-22 14:14:19 +0100

    cleaned up code

--- one_D_model/model/solve_SDE_stoch_stab_function.py
+++ one_D_model/model/solve_SDE_stoch_stab_function.py
@@ -2,9 +2,6 @@ import numpy as np
 import sdeint
 
 from one_D_model.model import solve_ODE
-from scipy.integrate import solve_ivp
-
-import matplotlib.pyplot as plt
 
 
 # First define the scaling functions that scale the SDE parameter values with Ri
@@ -69,73 +66,6 @@ def solve_SDE_with_stoch_stab_function(param):
 
     return result
 
-#
-# def define_poisson_stab_function(params, delta_T_val, U_val, critical_Ri=0.25):
-#     np.random.seed()
-#     # Define Poisson random variable
-#     poisson_rv = np.random.poisson(lam=params.poisson_lambda, size=1) * 0.1
-#     # Calculate Richardson number
-#     Ri_val = solve_ODE.calculate_richardson_number(params, delta_T_val, U_val)
-#     # Calculate stochastic stability function for given Richardson
-#     stab_function = solve_ODE.calculate_stability_function(params, delta_T_val, U_val)
-#     # if Ri_val <= critical_Ri:
-#     #     stab_function = solve_ODE.calculate_stability_function(params, delta_T_val, U_val)
-#     # else:
-#     #     stab_function = solve_ODE.calculate_stability_function(params, delta_T_val, U_val) + poisson_rv
-#     # if delta_T_val > 30:
-#     #     print(delta_T_val)
-#     #     print(poisson_rv)
-#     #     print(stab_function)
-#     #     print('---')
-#     # Limit stability function to 1
-#     # if stab_function > 1:
-#     #     stab_function = 1
-#
-#     Ri_list.append(Ri_val)
-#     poisson_list.append(poisson_rv)
-#     T_list.append(delta_T_val)
-#     stab_func_list.append(stab_function)
-#     return stab_function
-#
-#
-# def define_ODE_with_stoch_stab_func_poisson(t, delta_T, param):
-#     f_stab = define_poisson_stab_function(param, delta_T, param.U)
-#     c_D = (param.kappa / np.math.log(param.zr / param.z0)) ** 2
-#     return (1 / param.cv) * (
-#             param.Q_i - param.Lambda * delta_T - param.rho * param.cp * c_D * param.U * delta_T * f_stab)
-
-
-# def solve_ODE_with_stoch_stab_func_poisson(param):
-#     global Ri_list
-#     global poisson_list
-#     global T_list
-#     global stab_func_list
-#     Ri_list = []
-#     poisson_list = []
-#     T_list = []
-#     stab_func_list = []
-#
-#     solution = solve_ivp(define_ODE_with_stoch_stab_func_poisson, t_span=[param.t_start, param.t_end],
-#                          y0=[param.delta_T_0], t_eval=param.t_span, args=(param,))
-#
-#     print(len(T_list))
-#     print(np.shape(solution.y))
-#     print(len(param.t_span))
-#     fig, ax = plt.subplots(4, 1, figsize=(15, 20))
-#     axs = ax.ravel()
-#     axs[0].plot(T_list[200:400], "b-", label="solution")
-#     axs[1].plot(stab_func_list[200:400], "r-", label="stab_func")
-#     axs[2].plot(poisson_list[200:400], "g-", label="poisson")
-#     axs[3].scatter(Ri_list[200:400], stab_func_list[200:400])
-#
-#     axs[0].legend()
-#     axs[1].legend()
-#     axs[2].legend()
-#
-#     fig.savefig(f'solution.png', bbox_inches='tight', dpi=300)
-#
-#     return solution.y.flatten().reshape(-1, 1)
-
 
 def calculate_d_delta_T_d_t_with_poisson_stab_func(t, delta_T_val, param):
     """Define energy balance model with randomized stability function. It is randomized by adding poisson distributed
@@ -164,10 +94,6 @@ def calculate_d_delta_T_d_t_with_poisson_stab_func(t, delta_T_val, param):
     # else use the stochasticly modified one
     else:
         stab_function = solve_ODE.calculate_stability_function(param, delta_T_val, param.U) + poisson_rv
-
-    Ri_list.append(Ri_val)
-    poisson_list.append(poisson_rv)
-    stab_func_list.append(stab_function)
 
     c_D = (param.kappa / np.math.log(param.zr / param.z0)) ** 2
     return (1 / param.cv) * (
@@ -218,26 +144,4 @@ def solve_ODE_with_stoch_stab_func_poisson(param):
     Returns:
         numpy array: solution of energy balance model with random stability function
     """
-    global Ri_list
-    global poisson_list
-    global stab_func_list
-    Ri_list = []
-    poisson_list = []
-    stab_func_list = []
-
-    solution = define_runge_kutta_solver(param.t_start, param.delta_T_0, param)
-
-    fig, ax = plt.subplots(4, 1, figsize=(15, 20))
-    axs = ax.ravel()
-    axs[0].plot(solution, "b-", label="solution")
-    axs[1].plot(stab_func_list, "r-", label="stab_func")
-    axs[2].plot(poisson_list, "g-", label="poisson")
-    axs[3].scatter(Ri_list, stab_func_list)
-
-    axs[0].legend()
-    axs[1].legend()
-    axs[2].legend()
-
-    fig.savefig(f'solution.png', bbox_inches='tight', dpi=300)
-
-    return solution
+    return define_runge_kutta_solver(param.t_start, param.delta_T_0, param)
