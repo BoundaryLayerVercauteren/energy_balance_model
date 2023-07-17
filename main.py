d4d7e583c1b76d765cf165e14e3a3be581c8810f
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-07-17 14:55:43 +0200

    refactored code to improve readability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Main script to run energy balance model (with perturbations).
+Main script to run conceptual model for temperature inversions (with and without perturbations).
 """
 import os
 import sys
@@ -40,167 +40,155 @@ def create_u_range(param_vals, num_u_steps=50):
     return u.flatten()
 
 
-def run_model(param, function=False, stab_function=False, Qi=False, Lambda=False, u=False, make_plot=False,
-              u_and_function=False, stab_function_and_time_dependent_u=False, sensitivity_study=False,
-              ode_with_var_u=False):
-    # If command line flag is given make plots
-    if make_plot:
-        # Set plotting style and font sizes for figures
-        set_plotting_style.configure_plotting_style(figure_type='full_page_width')
-        # Process Dome C data and plot it
-        data_domec = process_dome_c_data.main()
-        # -------------------------------------------------------------------------------------
-        # # Solve deterministic ODE
-        # ODE_sol = solve_ODE.solve_deterministic_ODE(param)
-        # # Plot solution of deterministic model
-        # plot.make_2D_plot(param, ODE_sol.t.flatten(), ODE_sol.y.flatten(), 'ODE_sol.png')
-        # # Plot stability functions
-        #compare_stability_functions.make_comparison(param.sol_directory_path)
+def make_ode_plots(param):
+    # Set plotting style and font sizes for figures
+    set_plotting_style.configure_plotting_style(figure_type='full_page_width')
 
-        # -------------------------------------------------------------------------------------
-        # Make bifurcation plots
-        # copy dataclass to prevent overwriting original
-        param_copy = dataclasses.replace(param)
-        param_copy.sol_directory_path = param.sol_directory_path
-        param_copy.stab_func_type = 'short_tail'
-        make_bifurcation_analysis.make_bifurcation_analysis(param_copy, data_domec)#, save_values=True)
-        param_copy.stab_func_type = 'long_tail'
-        make_bifurcation_analysis.make_bifurcation_analysis(param_copy)#, data_domec)
-        # -------------------------------------------------------------------------------------
-        # Plot potential
-        #plot.plot_potentials(param)
-    # -----------------------------------------------------------------------------------------
-    if ode_with_var_u:
-        param.u_range = create_u_range(param)
-        if param.delta_T_0 < 12:
-            param.u_range = param.u_range[::-1]
-        np.savetxt(params.sol_directory_path + 'u_range.txt', param.u_range)
-        ode_sol = solve_ODE.solve_ODE_with_time_dependent_u(param)
-        np.save(params.sol_directory_path + 'ODE_sol_delta_T.npy', ode_sol)
-    # -----------------------------------------------------------------------------------------
-    # Run model with randomizations
-    # Which parameters are parameterized is specified by the command line flags
+    # Process Dome C data
+    data_domec = process_dome_c_data.main()
 
-    if sensitivity_study:
-        u_step_size = 0.1
-        u_range = np.round(np.arange(param.u_range_start, param.u_range_end + u_step_size, u_step_size), 3)
-        sigma_step_size = 0.02
-        sigma_range = np.round(np.arange(sigma_step_size, .5 + sigma_step_size, sigma_step_size), 3)
-        orig_sol_directory_path = params.sol_directory_path
-        for trans_type in ['weakly_very', 'very_weakly']:
-            cur_sol_directory_path = orig_sol_directory_path + f'{trans_type}/'
-            os.makedirs(cur_sol_directory_path)
-            if trans_type == 'weakly_very':
-                params.delta_T_0 = 4
-            else:
-                params.delta_T_0 = 24
-            if function:
-                for u_val in u_range:
-                    for sigma_val in sigma_range:
-                        params.sol_directory_path = cur_sol_directory_path + f"{str(u_val).replace('.', '_')}/{str(sigma_val).replace('.', '_')}/"
-                        os.makedirs(params.sol_directory_path)
-                        os.makedirs(params.sol_directory_path + 'temporary/')
-                        param.U = u_val
-                        param.sigma_delta_T = sigma_val
-                        run_1D_SDE_model.solve_randomized_model(param)
-                        # Save parameters
-                        save_parameters_in_file(param)
-            elif stab_function:
-                for u_val in u_range:
-                    for sigma_val in sigma_range:
-                        params.sol_directory_path = cur_sol_directory_path + f"{str(u_val).replace('.', '_')}/{str(sigma_val).replace('.', '_')}/"
-                        os.makedirs(params.sol_directory_path)
-                        os.makedirs(params.sol_directory_path + 'temporary/')
-                        param.U = u_val
-                        param.sigma_phi = sigma_val
-                        # Randomize wind velocity
-                        function_name = solve_SDE_stoch_stab_function.solve_SDE_with_stoch_stab_function_multi_noise
-                        sol_file_name = 'SDE_stab_func_multi_noise_sol'
-                        run_1D_SDE_model.solve_model_with_randomized_parameter(param, function_name, sol_file_name)
-                        # Save parameters
-                        save_parameters_in_file(param)
-            elif u_and_function:
-                sigma_u_range = np.array([0.01, 0.04])
-                sigma_range = np.insert(sigma_range, 0, 0, axis=0)
-                for u_val in u_range:
-                    for sigma_deltaT_val in sigma_range:
-                        for sigma_u_val in sigma_u_range:
-                            params.sol_directory_path = cur_sol_directory_path + f"{str(u_val).replace('.', '_')}/{str(sigma_deltaT_val).replace('.', '_')}/{str(sigma_u_val).replace('.', '_')}/"
-                            os.makedirs(params.sol_directory_path)
-                            os.makedirs(params.sol_directory_path + 'temporary/')
-                            param.U = u_val
-                            param.sigma_u = sigma_u_val
-                            param.sigma_delta_T = sigma_deltaT_val
-                            # Randomize both wind velocity and the whole model
-                            function_name = solve_SDEs.solve_SDE_with_stoch_u_and_internal_var
-                            sol_file_name = 'SDE_u_internal_var_sol'
-                            run_1D_SDE_model.solve_model_with_randomized_parameter(param, function_name, sol_file_name)
-                            # Save parameters
-                            save_parameters_in_file(param)
-        return
+    # Solve deterministic ODE
+    ODE_sol = solve_ODE.solve_deterministic_ODE(param)
+    # Plot solution of deterministic model
+    plot.make_2D_plot(param, ODE_sol.t.flatten(), ODE_sol.y.flatten(), 'ODE_sol.png')
 
-    if function:
-        # Randomize the whole model
+    # Plot stability functions
+    compare_stability_functions.make_comparison(param.sol_directory_path)
+
+    # Make bifurcation plots
+    # copy dataclass to prevent overwriting original
+    param_copy = dataclasses.replace(param)
+    param_copy.sol_directory_path = param.sol_directory_path
+    param_copy.stab_func_type = 'short_tail'
+    make_bifurcation_analysis.make_bifurcation_analysis(param_copy, data_domec, save_values=True)
+    param_copy.stab_func_type = 'long_tail'
+    make_bifurcation_analysis.make_bifurcation_analysis(param_copy, data_domec)
+
+    # Plot potential
+    plot.plot_potentials(param)
+
+
+def solve_ode_with_variable_u(param):
+    param.u_range = create_u_range(param)
+    if param.delta_T_0 < 12:
+        param.u_range = param.u_range[::-1]
+    np.savetxt(param.sol_directory_path + 'u_range.txt', param.u_range)
+    ode_sol = solve_ODE.solve_ODE_with_time_dependent_u(param)
+    np.save(param.sol_directory_path + 'ODE_sol_delta_T.npy', ode_sol)
+
+
+def run_sde_model(param, **randomization_type):
+    """Run conceptual model for temperature inversions with different types of randomizations. See section 2.3 and its
+     subsections."""
+    if randomization_type.get('function'):
+        # Solve model with small-scale fluctuations of an unresolved process (eq. 3)
         run_1D_SDE_model.solve_randomized_model(param)
-
-    if Qi:
-        # Randomize isothermal net radiation
-        function_name = solve_SDEs.solve_SDE_with_stoch_Qi
-        sol_file_name = 'SDE_Qi_sol'
-        run_1D_SDE_model.solve_model_with_randomized_parameter(param, function_name, sol_file_name)
-
-    if Lambda:
-        # Randomize lumped parameter for feedbacks from both soil heat conduction and radiative cooling
-        function_name = solve_SDEs.solve_SDE_with_stoch_lambda
-        sol_file_name = 'SDE_lambda_sol'
-        run_1D_SDE_model.solve_model_with_randomized_parameter(param, function_name, sol_file_name)
-
-    if u:
-        # Randomize wind velocity
-        function_name = solve_SDEs.solve_SDE_with_stoch_u
-        sol_file_name = 'SDE_u_sol'
-        run_1D_SDE_model.solve_model_with_randomized_parameter(param, function_name, sol_file_name)
-
-    if u_and_function:
-        # Randomize both wind velocity and the whole model
-        function_name = solve_SDEs.solve_SDE_with_stoch_u_and_internal_var
-        sol_file_name = 'SDE_u_internal_var_sol'
-        run_1D_SDE_model.solve_model_with_randomized_parameter(param, function_name, sol_file_name)
-
-    if stab_function:
-        # Randomize stability function
-        function_name = solve_SDE_stoch_stab_function.solve_SDE_with_stoch_stab_function_multi_noise
-        sol_file_name = 'SDE_stab_func_multi_noise_sol'
-        run_1D_SDE_model.solve_model_with_randomized_parameter(param, function_name, sol_file_name)
-
-    if stab_function_and_time_dependent_u:
-        # Randomize stability function and make wind velocity time dependent rather than one fixed value
-        # Calculate u range and save it
-        param.u_range = create_u_range(param)
-        if param.delta_T_0 < 12:
-            param.u_range = param.u_range[::-1]
-        np.savetxt(str(params.sol_directory_path) + 'u_range.txt', param.u_range)
-        function_name = solve_SDE_stoch_stab_function.solve_SDE_with_stoch_stab_function_multi_noise_time_dependent_u
-        sol_file_name = 'SDE_stab_func_multi_noise_sol'
-        run_1D_SDE_model.solve_model_with_randomized_parameter(param, function_name, sol_file_name)
-
-    # -----------------------------------------------------------------------------------------
+    if randomization_type.get('wind'):
+        # Solve model with small-scale fluctuations of the wind forcing (eq. 4)
+        run_1D_SDE_model.solve_model_with_randomized_parameter(param, solve_SDEs.solve_SDE_with_stoch_u, 'SDE_u_sol')
+    if randomization_type.get('wind_and_function'):
+        # Solve model with the previous two randomizations combined (eq. 5)
+        run_1D_SDE_model.solve_model_with_randomized_parameter(param,
+                                                               solve_SDEs.solve_SDE_with_stoch_u_and_internal_var,
+                                                               'SDE_u_internal_var_sol')
+    if randomization_type.get('stab_function'):
+        # Solve model with a stochastic stability function (eq. 6)
+        run_1D_SDE_model.solve_model_with_randomized_parameter(param,
+                                                               solve_SDE_stoch_stab_function.solve_SDE_with_stoch_stab_function,
+                                                               'SDE_stab_func_sol')
     # Save parameters
     save_parameters_in_file(param)
 
 
+def run_sde_model_with_nonconstant_wind(param, **randomization_type):
+    param.u_range = create_u_range(param)
+    if param.delta_T_0 < 12:
+        param.u_range = param.u_range[::-1]
+    np.savetxt(str(param.sol_directory_path) + 'u_range.txt', param.u_range)
+
+    if randomization_type.get('stab_function'):
+        run_1D_SDE_model.solve_model_with_randomized_parameter(param,
+                                                               solve_SDE_stoch_stab_function.solve_SDE_with_stoch_stab_function_time_dependent_u,
+                                                               'SDE_stab_func_sol')
+
+
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        os.makedirs(path + 'temporary/')
+
+
+def make_sensitivity_study_for_sde_model(param, **randomization_type):
+    u_step_size = 0.1
+    u_range = np.round(np.arange(param.u_range_start, param.u_range_end + u_step_size, u_step_size), 3)
+    sigma_step_size = 0.02
+    sigma_range = np.round(np.arange(sigma_step_size, .5 + sigma_step_size, sigma_step_size), 3)
+    orig_sol_directory_path = params.sol_directory_path
+    for trans_type in ['weakly_very', 'very_weakly']:
+        cur_sol_directory_path = orig_sol_directory_path + f'{trans_type}/'
+        os.makedirs(cur_sol_directory_path)
+        if trans_type == 'weakly_very':
+            params.delta_T_0 = 4
+        else:
+            params.delta_T_0 = 24
+        if randomization_type.get('function'):
+            for u_val in u_range:
+                for sigma_val in sigma_range:
+                    params.sol_directory_path = cur_sol_directory_path + f"{str(u_val).replace('.', '_')}/{str(sigma_val).replace('.', '_')}/"
+                    create_directory(params.sol_directory_path)
+                    param.U = u_val
+                    param.sigma_delta_T = sigma_val
+                    run_sde_model(param, function=True)
+        if randomization_type.get('stab_function'):
+            for u_val in u_range:
+                for sigma_val in sigma_range:
+                    params.sol_directory_path = cur_sol_directory_path + f"{str(u_val).replace('.', '_')}/" \
+                                                                         f"{str(sigma_val).replace('.', '_')}/"
+                    create_directory(params.sol_directory_path)
+                    param.U = u_val
+                    param.sigma_phi = sigma_val
+                    run_sde_model(param, stab_function=True)
+        if randomization_type.get('wind_and_function'):
+            sigma_u_range = np.array([0.01, 0.04])
+            sigma_range = np.insert(sigma_range, 0, 0, axis=0)
+            for u_val in u_range:
+                for sigma_deltaT_val in sigma_range:
+                    for sigma_u_val in sigma_u_range:
+                        params.sol_directory_path = cur_sol_directory_path + f"{str(u_val).replace('.', '_')}/" \
+                                                                             f"{str(sigma_deltaT_val).replace('.', '_')}" \
+                                                                             f"/{str(sigma_u_val).replace('.', '_')}/"
+                        create_directory(params.sol_directory_path)
+                        param.U = u_val
+                        param.sigma_u = sigma_u_val
+                        param.sigma_delta_T = sigma_deltaT_val
+                        run_sde_model(param, wind_and_function=True)
+
+
 if __name__ == "__main__":
     # Read command line input
-    f, sf, Q_i, Lam, wind, mp, uf, sfu, ss, odeu = parse_command_line_input.read_command_line_input()
+    f, sf, u, pl, uf, sfu, ss, odeu = parse_command_line_input.read_command_line_input()
     # -----------------------------------------------------------------------------------------
     # Load Parameters
     params = parameters.Parameters()
     # -----------------------------------------------------------------------------------------
     # Make directory for output
     params.sol_directory_path = 'output' + '/' + time.strftime("%Y%m%d_%H%M_%S") + '/'
-    if not os.path.exists(params.sol_directory_path):
-        os.makedirs(params.sol_directory_path)
-        os.makedirs(params.sol_directory_path + 'temporary/')
+    create_directory(params.sol_directory_path)
     # -----------------------------------------------------------------------------------------
-    # Run model and save output
-    run_model(params, f, sf, Q_i, Lam, wind, mp, uf, sfu, ss, odeu)
+    # Create plots for ODE model
+    if pl:
+        make_ode_plots(params)
+    # Solve ODE with variable u
+    if odeu:
+        solve_ode_with_variable_u(params)
+    # -----------------------------------------------------------------------------------------
+    # Run sensitivity analysis for SDE model
+    if ss:
+        make_sensitivity_study_for_sde_model(params, function=f, wind_and_function=uf, stab_function=sf)
+    # Run SDE model (for one fixed wind velocity)
+    else:
+        run_sde_model(params, function=f, wind=u, wind_and_function=uf, stab_function=sf)
+    # Run SDE model with time dependent wind velocity
+    if sfu:
+        run_sde_model_with_nonconstant_wind(params, stab_function=sfu)
+
