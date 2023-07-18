9d7e3d07dbaa9f0b01061bcc02901d91d12d8954
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-07-18 14:48:56 +0200

    restructered code to improve readability, added comments

--- one_D_model/model/run_SDE_model.py
+++ one_D_model/model/run_SDE_model.py
@@ -1,3 +1,5 @@
+"""Script to perform Monte Carlo simulation of SDEs (eq. 3-6)."""
+
 import multiprocessing
 import os
 from functools import partial
@@ -7,6 +9,7 @@ from one_D_model.model import solve_SDEs
 
 
 def solve_SDEs_wrapper(_, func_name, param):
+    """Wrapper function to transform SDE results into desired shape."""
     values = func_name(param)
     if np.shape(values)[1] > 1:
         return values[:, 0], values[:, 1]
@@ -15,6 +18,7 @@ def solve_SDEs_wrapper(_, func_name, param):
 
 
 def combine_npy_files(num_sim, sol_directory_path, file_string):
+    """Combine all data files in the directory temporary to one and delete them."""
     solution = []
     path = sol_directory_path + 'temporary/'
     for idx in range(num_sim):
@@ -26,26 +30,31 @@ def combine_npy_files(num_sim, sol_directory_path, file_string):
 
 
 def solve_randomized_model(params):
+    """Run Monte Carlo simulation of conceptual model for temperature inversions with additive noise term to represent
+    small-scale fluctuations of an unresolved process (eq. 3). The simulations are run in parallel."""
     # Solve SDEs and save all solutions in a separate file
     with multiprocessing.Pool(processes=params.num_proc) as pool:
         for idx, res in enumerate(
-                pool.imap_unordered(partial(solve_SDEs_wrapper, func_name=solve_SDEs.solve_SDE, param=params),
-                                    range(params.num_simulation))):
-            np.save(params.sol_directory_path + 'temporary/SDE_sol_delta_T_' + str(idx) + '.npy', res)
+                pool.imap_unordered(
+                    partial(solve_SDEs_wrapper, func_name=solve_SDEs.solve_SDE_with_internal_var, param=params),
+                    range(params.num_simulation))):
+            np.save(params.sol_directory_path + 'temporary/SDE_internal_var_sol_delta_T_' + str(idx) + '.npy', res)
 
     # Combine solution files into a single one and delete single files
-    combine_npy_files(params.num_simulation, params.sol_directory_path, 'SDE_sol_delta_T')
+    combine_npy_files(params.num_simulation, params.sol_directory_path, 'SDE_internal_var_sol_delta_T')
 
 
 def solve_model_with_randomized_parameter(params, function_name, sol_file_name):
+    """Run Monte Carlo simulation of perturbed conceptual model for temperature inversions (eq. 4, 5, 6). The
+    simulations are run in parallel."""
     with multiprocessing.Pool(processes=params.num_proc) as pool:
         for idx, res in enumerate(pool.imap_unordered(
-                partial(solve_SDEs_wrapper, func_name=function_name, param=params),
-                range(params.num_simulation))):
+                partial(solve_SDEs_wrapper, func_name=function_name, param=params), range(params.num_simulation))):
             if np.shape(res)[1] > 1:
                 np.save(params.sol_directory_path + 'temporary/' + sol_file_name + '_delta_T_' + str(idx) + '.npy',
                         res[0][:])
-                np.save(params.sol_directory_path + 'temporary/' + sol_file_name + '_param_' + str(idx) + '.npy', res[1][:])
+                np.save(params.sol_directory_path + 'temporary/' + sol_file_name + '_param_' + str(idx) + '.npy',
+                        res[1][:])
             else:
                 np.save(params.sol_directory_path + 'temporary/' + sol_file_name + '_delta_T_' + str(idx) + '.npy',
                         res.flatten())
@@ -55,16 +64,3 @@ def solve_model_with_randomized_parameter(params, function_name, sol_file_name):
     if np.shape(res)[1] > 1:
         combine_npy_files(params.num_simulation, params.sol_directory_path, sol_file_name + '_param')
 
-
-# Very slow!
-def solve_model_with_randomized_parameter_z0(params, sol_file_name):
-    with multiprocessing.Pool(processes=params.num_proc) as pool:
-        for idx, res in enumerate(pool.imap_unordered(
-                partial(solve_SDEs.solve_SDE_with_stoch_z0, param=params), range(params.num_simulation))):
-            np.save(params.sol_directory_path + 'temporary/' + sol_file_name + '_delta_T_' + str(idx) + '.npy',
-                    res[0][:])
-            np.save(params.sol_directory_path + 'temporary/' + sol_file_name + '_param_' + str(idx) + '.npy', res[1][:])
-
-    # Combine solution files into a single one and delete single files
-    combine_npy_files(params.num_simulation, params.sol_directory_path, sol_file_name + '_delta_T')
-    combine_npy_files(params.num_simulation, params.sol_directory_path, sol_file_name + '_param')
