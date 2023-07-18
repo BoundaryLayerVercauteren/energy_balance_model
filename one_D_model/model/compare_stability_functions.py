f6eee3bbca1e84b12b720eba98008f238cc97ce1
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-07-18 16:10:04 +0200

    restructered code to improve readability, added comments

--- one_D_model/model/compare_stability_functions.py
+++ one_D_model/model/compare_stability_functions.py
@@ -1,41 +1,20 @@
+"""Script to compare  deterministic stability function (see section 2.2.)."""
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import sdeint
-import seaborn as sns
 
-import one_D_model.model.solve_SDE_stoch_stab_function as stoch_stab_function
+from one_D_model.utils.set_plotting_style import configure_plotting_style
 
-plt.style.use('science')
 
-# Set font sizes for plots
-SMALL_SIZE = 11
-MEDIUM_SIZE = 12
-BIGGER_SIZE = 15
-
-plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
-plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+# Set plotting style
+configure_plotting_style('full_page_width')
 
 
 def define_vandewiel_short_tail_stab_function(Ri, alpha=5):
     """Calculate value of short-tail stability function for given Richardson number (and alpha). The function was
     originally defined in Van de Wiel, B. J. H., and Coauthors, 2017: Regime transitions in near-surface temperature
     inversions: A conceptual model. Journal of the Atmospheric Sciences, 74, 1057–1073,
-    https://doi.org/10.1175/JAS-D-16-0180.1.
-
-    Args:
-        Ri (float): Richardson number.
-        alpha (float): Parameter for stability function.
-
-    Returns:
-        (float): value of short-tail stability function
-    """
+    https://doi.org/10.1175/JAS-D-16-0180.1."""
     return np.exp(-2 * alpha * Ri - (alpha * Ri) ** 2)
 
 
@@ -43,52 +22,18 @@ def define_vandewiel_long_tail_stab_function(Ri, alpha=5):
     """Calculate value of long-tail stability function for given Richardson number (and alpha). The function was
     originally defined in Van de Wiel, B. J. H., and Coauthors, 2017: Regime transitions in near-surface temperature
     inversions: A conceptual model. Journal of the Atmospheric Sciences, 74, 1057–1073,
-    https://doi.org/10.1175/JAS-D-16-0180.1.
-
-    Args:
-        Ri (float): Richardson number.
-        alpha (float): Parameter for stability function.
-
-    Returns:
-        (float): value of long-tail stability function
-    """
+    https://doi.org/10.1175/JAS-D-16-0180.1."""
     return np.exp(-2 * alpha * Ri)
 
 
-def define_vandewiel_cutoff_stab_function(Ri, alpha=5):
-    """Calculate value of cutoff stability function for given Richardson number (and alpha). The function was
-    originally defined in Van de Wiel, B. J. H., and Coauthors, 2017: Regime transitions in near-surface temperature
-    inversions: A conceptual model. Journal of the Atmospheric Sciences, 74, 1057–1073,
-    https://doi.org/10.1175/JAS-D-16-0180.1.
-
-    Args:
-        Ri (float): Richardson number.
-        alpha (float): Parameter for stability function.
-
-    Returns:
-        (float): value of cutoff stability function
-    """
-    cutoff = 1 / alpha
-    if Ri <= cutoff:
-        return 1 - 2 * alpha * Ri
-    else:
-        return 0.0
-
-
 def make_comparison(sol_directory_path):
-    """
-    Create plot to compare different stablity functions.
-
-    Args:
-        sol_directory_path (str): Path were figure should be saved.
-    """
+    """Create plot to compare different stability functions."""
     # Define range of Richardson numbers at which function will be evaluated
     richardson_num = np.round(np.linspace(0, 0.5, 100), 4)
 
     # Calculate stability function values for a range of Richardson numbers
     vec_vandewiel_short_tail_stab_func = np.vectorize(define_vandewiel_short_tail_stab_function)
     vec_vandewiel_long_tail_stab_func = np.vectorize(define_vandewiel_long_tail_stab_function)
-    vec_vandewiel_cutoff_stab_func = np.vectorize(define_vandewiel_cutoff_stab_function)
 
     # Create plot
     color = matplotlib.cm.get_cmap('cmc.batlow', 3).colors
@@ -101,12 +46,9 @@ def make_comparison(sol_directory_path):
              color=color[0], marker=markers[0], markevery=10)
     ax1.plot(richardson_num, vec_vandewiel_long_tail_stab_func(richardson_num), label='long tail',
              color=color[1], marker=markers[1], markevery=10)
-    # ax1.plot(richardson_num, vec_vandewiel_cutoff_stab_func(richardson_num), label='cutoff',
-    #          color=color[2], marker=markers[2], markevery=100)
 
     ax1.set_xlabel(r'$R_b$')
     ax1.set_ylabel(r'$f_{stab}$')
-    #ax1.set_xscale('log')
 
     plt.legend()
 
