49af3c3fb23b6f491d77704bc2b41305eb8e76e2
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-07-18 16:11:11 +0200

    added option to specify font sizes in function call

--- one_D_model/utils/set_plotting_style.py
+++ one_D_model/utils/set_plotting_style.py
@@ -1,22 +1,22 @@
 from matplotlib import pyplot as plt
 
 
-def configure_plotting_style(figure_type):
+def configure_plotting_style(figure_type, small_size=None, medium_size=None, large_size=None):
     plt.style.use('science')
     # set font sizes for plots
     if figure_type == 'full_page_width':
-        SMALL_SIZE = 11
-        MEDIUM_SIZE = 12
-        BIGGER_SIZE = 15
+        small_size = 11
+        medium_size = 12
+        large_size = 15
     elif figure_type == 'half_page_width':
-        SMALL_SIZE = 14
-        MEDIUM_SIZE = 15
-        BIGGER_SIZE = 19
+        small_size = 14
+        medium_size = 15
+        large_size = 19
 
-    plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
-    plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
-    plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
-    plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
-    plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
-    plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
-    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+    plt.rc('font', size=small_size)  # controls default text sizes
+    plt.rc('axes', titlesize=small_size)  # fontsize of the axes title
+    plt.rc('axes', labelsize=medium_size)  # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=small_size)  # fontsize of the tick labels
+    plt.rc('ytick', labelsize=small_size)  # fontsize of the tick labels
+    plt.rc('legend', fontsize=small_size)  # legend fontsize
+    plt.rc('figure', titlesize=large_size)  # fontsize of the figure title
