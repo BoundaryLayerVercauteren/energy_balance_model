9c48a4a66783404f2b264ca577060b482c707267
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-07-19 13:14:19 +0200

    restructered code to improve readability

--- one_D_model/post_processing/plot_simulation_output.py
+++ one_D_model/post_processing/plot_simulation_output.py
@@ -1,45 +1,40 @@
-import numpy as np
-from dataclasses import dataclass
-from dataclasses_json import dataclass_json
+"""Needs to be run with: python -m one_D_model.post_processing.plot_simulation_output"""
+import dataclasses
 import os
+from dataclasses import dataclass
 
-from one_D_model.utils import plot_output as plot
+import matplotlib
+import numpy as np
+from dataclasses_json import dataclass_json
+from matplotlib import pyplot as plt
+import cmcrameri as cmc
+
+from one_D_model.model import solve_ODE
+from one_D_model.utils import plot_output
 
 # Define directory where simulation output is saved
-output_directory = 'output/vw/0_01/'
-#
-# from one_D_model.model import run_SDE_model
-# import os
-#
-# _, _, files = next(os.walk(output_directory+ 'temporary/'))
-# num_sim = len(files)
-# run_SDE_model.combine_npy_files(num_sim, 'output/20230322_1422_31/', 'SDE_stab_func_poisson_sol_delta_T')
+output_directory = 'output/20230719_1159_24/'
+
+# Specify what type of simulation was run and shall be plotted
+sim_type_options = ['internal_var', 'wind', 'internal_var_wind', 'turbulence']
+sim_type = sim_type_options[3]
 
 # Load data
-#SDE_sol_delta_T = np.load(output_directory + 'SDE_sol_delta_T.npy')
-
-# SDE_u_sol_delta_T = np.load(output_directory + 'SDE_u_sol_delta_T.npy')
-# SDE_u_sol_u = np.load(output_directory + 'SDE_u_sol_param.npy')
-
-SDE_u_sol_delta_T = np.load(output_directory + 'SDE_u_internal_var_sol_delta_T.npy')
-SDE_u_sol_u = np.load(output_directory + 'SDE_u_internal_var_sol_param.npy')
-#
-# SDE_Qi_sol_delta_T = np.load(output_directory + 'SDE_Qi_sol_delta_T.npy')
-# SDE_Qi_sol_Qi = np.load(output_directory + 'SDE_Qi_sol_param.npy')
-#
-# SDE_lambda_sol_delta_T = np.load(output_directory + 'SDE_lambda_sol_delta_T.npy')
-# SDE_lambda_sol_lambda = np.load(output_directory + 'SDE_lambda_sol_param.npy')
-#
-# SDE_stab_func_sol_delta_T = np.load(output_directory + 'SDE_stab_func_sol_delta_T.npy')
-# SDE_stab_func_sol_sf = np.load(output_directory + 'SDE_stab_func_sol_param.npy')
-#
-# SDE_stab_func_sol_poisson_delta_T = np.load(output_directory + 'SDE_stab_func_poisson_sol_delta_T.npy')
-
-# SDE_z0_sol_delta_T = np.load(output_directory + 'SDE_z0_sol_delta_T.npy', allow_pickle=True)
-# SDE_z0_sol_z0 = np.load(output_directory + 'SDE_z0_sol_param.npy', allow_pickle=True)
-#
-# SDE_stab_func_sol_delta_T = np.load(output_directory + 'SDE_stab_func_multi_noise_sol_delta_T.npy')
-# SDE_stab_func_sol_sf = np.load(output_directory + 'SDE_stab_func_multi_noise_sol_param.npy')
+if sim_type == sim_type_options[0]:
+    file_name_delta_T = 'SDE_internal_var_sol_delta_T'
+    SDE_sol_delta_T = np.load(output_directory + file_name_delta_T + '.npy')
+else:
+    if sim_type == sim_type_options[1]:
+        file_name_delta_T = 'SDE_u_sol_delta_T'
+        file_name_param = 'SDE_u_sol_param'
+    elif sim_type == sim_type_options[2]:
+        file_name_delta_T = 'SDE_u_internal_var_sol_delta_T'
+        file_name_param = 'SDE_u_internal_var_sol_param'
+    elif sim_type == sim_type_options[3]:
+        file_name_delta_T = 'SDE_stab_func_sol_delta_T'
+        file_name_param = 'SDE_stab_func_sol_param'
+    SDE_sol_delta_T = np.load(output_directory + file_name_delta_T + '.npy')
+    SDE_sol_param = np.load(output_directory + file_name_param + '.npy')
 
 
 # Load parameters
@@ -71,72 +66,109 @@ with open(output_directory + 'parameters.json', 'r') as file:
 
 params = Parameters.from_json(param_data)
 
-params.sol_directory_path = output_directory.rsplit('/', 2)[0] + '/'
 params.t_span_h = np.linspace(params.t_start, params.t_end_h, params.num_steps)
 
 # Make directory for visualizations
-if not os.path.exists(params.sol_directory_path + 'visualizations/'):
-    os.makedirs(params.sol_directory_path + 'visualizations/')
+params.sol_directory_path = output_directory + 'visualizations/'
+if not os.path.exists(params.sol_directory_path):
+    os.makedirs(params.sol_directory_path)
 
-# Make distribution plots
-# plot.make_distribution_plot(np.array(SDE_sol_delta_T).flatten(), params, 'visualizations/SDE_sol_delta_T_distribution.pdf',
-#                             r'$\Delta T$ [K]')
-plot.make_distribution_plot(SDE_u_sol_delta_T.flatten(), params, 'visualizations/SDE_u_sol_delta_T_distribution.pdf',
-                            r'$\Delta T$ [K]')
-# plot.make_distribution_plot(SDE_Qi_sol_delta_T.flatten(), params, 'visualizations/SDE_Qi_sol_delta_T_distribution.pdf',
-#                             r'$\Delta T$ [K]')
-# plot.make_distribution_plot(SDE_lambda_sol_delta_T.flatten(), params, 'visualizations/SDE_lambda_sol_delta_T_distribution.pdf',
-#                             r'$\Delta T$ [K]')
-# plot.make_distribution_plot(SDE_stab_func_sol_delta_T.flatten(), params, 'visualizations/SDE_stab_func_sol_delta_T_distribution.pdf',
-#                             r'$\Delta T$ [K]')
-# plot.make_distribution_plot(SDE_stab_func_sol_poisson_delta_T.flatten(), params, 'visualizations/SDE_stab_func_sol_delta_T_distribution.pdf',
-#                             r'$\Delta T$ [K]')
-# plot.make_distribution_plot(SDE_z0_sol_delta_T.flatten(), params, 'visualizations/SDE_z0_sol_delta_T_distribution.pdf',
-#                             r'$\Delta T$ [K]')
+
+def make_distribution_plot(values, params, file_name, xlabel):
+    fig = plt.figure(figsize=(5, 5))
+
+    color = matplotlib.cm.get_cmap('cmc.batlow', 1).colors
+
+    plt.axvline(x=24, color='r')
+    plt.axvline(x=4, color='r')
+    plt.axvline(x=12, color='r', linestyle='--')
+    plt.hist(values, 100, color=color[0])
+    #plt.ylim((0, 3.25 * 10 ** 6))
+    plt.xlabel(xlabel)
+    plt.ylabel(r'Density of $\Delta T$')
+    plt.savefig(params.sol_directory_path + file_name, bbox_inches='tight', dpi=300)
+    # To clear memory
+    plt.cla()  # Clear the current axes.
+    plt.clf()  # Clear the current figure.
+    plt.close('all')  # Closes all the figure windows.
+
+
+# Plot distribution of Delta T solution
+make_distribution_plot(SDE_sol_delta_T.flatten(), params, 'distribution_' + file_name_delta_T,
+                       r'$\Delta T$ [K]')
+
 
 # Overlay potential plot with distribution plot
-#plot.plot_potentials_and_output_distribution(params, SDE_sol_delta_T.flatten())
-exit()
+def plot_potentials_and_output_distribution(param_class, delta_T_data, file_str):
+    delta_T_range = np.arange(0, 30, 0.5)
+    u_list_st = [5.3, 5.6, 5.9]
+    u_list_lt = [4.87, 4.89, 4.9]
+
+    color = matplotlib.cm.get_cmap('cmc.batlow', 4).colors
+    markers = ['s', 'p', 'v']
+
+    # copy dataclass to prevent overwriting original
+    param_copy = dataclasses.replace(param_class)
+
+    fig, ax1 = plt.subplots(1, 1, figsize=(5, 5))
+    fig.subplots_adjust(right=0.75)
+
+    ax2 = ax1.twinx()
+
+    if param_copy.stab_func_type == 'short_tail':
+        p0 = ax2.hist(delta_T_data, 100, color=color[1], alpha=0.5, zorder=0)
+        for idx, u_elem in enumerate(u_list_st):
+            potential_st = solve_ODE.calculate_potential(delta_T_range, u_elem, param_copy)
+            p1, = ax1.plot(delta_T_range, - potential_st, label='u = ' + str(u_elem), color=color[idx],
+                           marker=markers[idx], markevery=5, zorder=10)
+        title = 'a)'
+
+    elif param_copy.stab_func_type == 'long_tail':
+        p2 = ax2.hist(delta_T_data, 100, color=color[1], alpha=0.5, zorder=0)
+        for idx, u_elem in enumerate(u_list_lt):
+            potential_lt = solve_ODE.calculate_potential(delta_T_range, u_elem, param_copy)
+            p3, = ax1.plot(delta_T_range, - potential_lt, label='u = ' + str(u_elem), color=color[idx],
+                           marker=markers[idx], markevery=5, zorder=10)
+        title = 'b)'
+
+    ax1.set_xlabel(r'$\Delta T$ [K]')
+    ax1.set_ylabel('V [$K^2$/s]')
+    ax2.set_ylabel(r'Density of $\Delta T$', color=color[1], alpha=1)
+    ax2.tick_params(axis="y", labelcolor=color[1])
+
+    #ax2.set_ylim((0.0, 4.5 * 10 ** 6))
+    ax1.set_title(title, loc='left')
+    ax1.legend()  # loc='upper left')
+
+    plt.savefig(f'{param_class.sol_directory_path}{file_str}_dist_potentials_{param_copy.stab_func_type}.pdf',
+                bbox_inches='tight', dpi=300)
+
+
+plot_potentials_and_output_distribution(params, SDE_sol_delta_T.flatten(), file_name_delta_T)
+
 # Plot ten time series for every simulation type
-if params.num_simulation >= 10:
+if params.num_simulation >= 1:
     vis_idx = np.linspace(0, params.num_simulation - 1, 10).astype(int)
     for idx in vis_idx:
-        # plot.make_2D_plot(params, params.t_span_h, SDE_sol_delta_T[idx][:].flatten(),
-        #                   'visualizations/SDE_sol_delta_T_over_time_sim' + str(idx) + '.pdf', xlabel='t [h]',
-        #                   ylabel=r'$\Delta T$ [K]')
-        #
-        # plot.make_2D_multi_line_plot(params, params.t_span_h,
-        #                              np.array([SDE_u_sol_delta_T[idx, :], SDE_u_sol_u[idx, :]]).T,
-        #                              [r'$\Delta T$', 'U'], 'visualizations/SDE_u_sol_u_delta_T_over_time_sim' + str(idx) + '.pdf',
-        #                              xlabel='t [h]', ylabel=r'$\Delta T$ [K]', ylabel2=r'U [$\mathrm{ms^{-1}}$]')
-        #
-        # plot.make_2D_multi_line_plot(params, params.t_span_h,
-        #                              np.array([SDE_Qi_sol_delta_T[idx, :], 1/10*SDE_Qi_sol_Qi[idx, :]]).T,
-        #                              [r'$\Delta T$', r'$0.1*Q_i$'],
-        #                              'visualizations/SDE_Qi_sol_Qi_delta_T_over_time_sim' + str(idx) + '.pdf',
-        #                              xlabel='t [h]',
-        #                              ylabel=r'$\Delta T$ [K]')
-        #
-        # plot.make_2D_multi_line_plot(params, params.t_span_h,
-        #                              np.array([SDE_lambda_sol_delta_T[idx, :], SDE_lambda_sol_lambda[idx, :]]).T,
-        #                              [r'$\Delta T$', r'$\lambda$'],
-        #                              'visualizations/SDE_lambda_sol_lambda_delta_T_over_time_sim' + str(idx) + '.pdf',
-        #                              xlabel='t [h]',
-        #                              ylabel=r'$\Delta T$ [K]')
-        #
-        plot.make_2D_multi_line_plot(params, params.t_span_h,
-                                     np.array([SDE_stab_func_sol_delta_T[idx, :], SDE_stab_func_sol_sf[idx, :]]).T,
-                                     [r'$\Delta T$', r'$\phi$'],
-                                     f'visualizations/SDE_stab_func_sol_sf_delta_T_over_time_sim{idx}.pdf',
-                                     xlabel='t [h]', ylabel=r'$\Delta T$ [K]', ylabel2=r'$\phi$')
-        #
-        # plot.make_2D_plot(params, params.t_span_h, SDE_stab_func_sol_poisson_delta_T[idx][:].flatten(),
-        #                   'visualizations/SDE_sol_delta_T_over_time_sim' + str(idx) + '.pdf', xlabel='t [h]',
-        #                   ylabel=r'$\Delta T$ [K]')
+        if sim_type == sim_type_options[0]:
+            plot_output.make_line_plot_of_single_solution(params, params.t_span_h, SDE_sol_delta_T[idx][:].flatten(),
+                                                          'SDE_sol_delta_T_over_time_sim' + str(
+                                                              idx) + '.pdf', xlabel='t [h]',
+                                                          ylabel=r'$\Delta T$ [K]')
+        else:
+            if sim_type == sim_type_options[1] or sim_type == sim_type_options[2]:
+                line_labels = [r'$\Delta T$', 'U']
+                y_axes_label_1 = r'$\Delta T$ [K]'
+                y_axes_label_2 = r'U [$\mathrm{ms^{-1}}$]'
+            elif sim_type == sim_type_options[3]:
+                line_labels = [r'$\Delta T$', r'$\phi$']
+                y_axes_label_1 = r'$\Delta T$ [K]'
+                y_axes_label_2 = r'$\phi$'
 
-        # plot.make_2D_multi_line_plot(params, params.t_span_h,
-        #                              np.array([SDE_z0_sol_delta_T[idx, 1:], 100*SDE_z0_sol_z0[idx, 1:]]).T,
-        #                              [r'$\Delta T$', r'$100*z_0$'],
-        #                              'visualizations/SDE_z0_sol_z0_delta_T_over_time_sim' + str(idx) + '.pdf',
-        #                              xlabel='t [h]',
-        #                              ylabel=r'$\Delta T$ [K]')
+            plot_output.make_line_plot_of_solution_and_parameter(params, params.t_span_h,
+                                                                 np.array([SDE_sol_delta_T[idx, :],
+                                                                           SDE_sol_param[idx, :]]).T,
+                                                                 line_labels,
+                                                                 f'{file_name_delta_T}_over_time_sim{idx}.pdf',
+                                                                 xlabel='t [h]', ylabel=y_axes_label_1,
+                                                                 ylabel2=y_axes_label_2)
